d22272a3e932504e3d6575ee83138ce2816bef67
SuperMardle <matao.mardle@gmail.com>
2016-10-15 23:02:48 +0800

    [ADD] Add Q4

--- Java/src/main/java/Part1/Q4.java
+++ Java/src/main/java/Part1/Q4.java
@@ -2,6 +2,20 @@ package Part1;
 
 /**
  * Created by matao on 10/15/16.
+ *
+ * <p>
+ * Write a method to replace all spaces in a string with'%20'.
+ * You may assume that the string has sufficient space at the end of the string
+ * to hold the additional characters,
+ * and that you are given the "true" length of the string.
+ * (Note: if imple- menting in Java,
+ * please use a character array so that you can perform this opera- tion in place.)
+ *
+ * EXAMPLE
+ * Input: "Mr John Smith Output: "Mr%20Dohn%20Smith"
+ *
+ * 将字符串中的空格全部替换为"%20"，假定该字符串尾部有足够的空间存放新增字符，并且知道字符串的"真实"长度。
+ * Java请用字符数字实现。
  */
 public class Q4 {
 }
