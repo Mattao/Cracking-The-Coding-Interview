1ceac30da1dfe2bd8bb4e07c5385ad6ed85d27da
SuperMardle <matao.mardle@gmail.com>
2016-10-15 22:09:03 +0800

    [ADD] Integrate maven & add junit

--- Java/src/main/java/Part1/Q1.java
+++ Java/src/main/java/Part1/Q1.java
@@ -4,7 +4,12 @@ package Part1;
  * Created by matao on 10/15/16.
  */
 public class Q1 {
+
+    public int add(int numA, int numB) {
+        return numA + numB;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("xxx");
     }
 }
