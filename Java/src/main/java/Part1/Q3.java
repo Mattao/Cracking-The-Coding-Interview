d5c67838a00b24e6b82fddd5aa4fa2aaf739ab01
SuperMardle <matao.mardle@gmail.com>
2016-10-15 23:01:56 +0800

    [ADD] Add Q3

---
+++ Java/src/main/java/Part1/Q3.java
@@ -0,0 +1,10 @@
+package Part1;
+
+/**
+ * Created by matao on 10/15/16.
+ *
+ * Given two strings, write a method to decide if one is a permutation of the other.
+ * 给定两个字符串，确定其中一个字符串的字符重新排序后能否成为另一个字符串
+ */
+public class Q3 {
+}
---
+++ Java/src/main/java/Part1/Q4.java
@@ -0,0 +1,7 @@
+package Part1;
+
+/**
+ * Created by matao on 10/15/16.
+ */
+public class Q4 {
+}
