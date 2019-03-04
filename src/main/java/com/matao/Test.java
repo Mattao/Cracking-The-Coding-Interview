fcfb036a4eaefe91fadc878ae0e715d0039b4fc5
matao <muxter.matao@gmail.com>
2019-03-04 12:25:14 +0800

    feat(Test): add Test for hello-world

---
+++ src/main/java/com/matao/Test.java
@@ -0,0 +1,11 @@
+package com.matao;
+
+/**
+ * Created by matao on 2019-03-04.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
