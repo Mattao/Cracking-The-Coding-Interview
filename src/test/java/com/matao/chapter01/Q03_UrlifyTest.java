18565320205cee983f5721305eca73a779e09336
matao <muxter.matao@gmail.com>
2019-03-05 15:15:46 +0800

    test(Q03-Urlify): add test cases for Urlify

--- src/test/java/com/matao/chapter01/Q03_UrlifyTest.java
+++ src/test/java/com/matao/chapter01/Q03_UrlifyTest.java
@@ -1,4 +1,25 @@
 package com.matao.chapter01;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class Q03_UrlifyTest {
+
+    @Test
+    void testNull() {
+        Assertions.assertArrayEquals(null, Q03_Urlify.replaceSpaces(null, 0));
+    }
+
+    @Test
+    void testEmpty() {
+        System.out.println("".toCharArray().length);
+        Assertions.assertArrayEquals("".toCharArray(), Q03_Urlify.replaceSpaces("".toCharArray(), 0));
+    }
+
+    @Test
+    void test() {
+        String actual = "Mr John Smith";
+        String expected = "Mr%20John%20Smith";
+        Assertions.assertArrayEquals(expected.toCharArray(), Q03_Urlify.replaceSpaces(actual.toCharArray(), 13));
+    }
 }
