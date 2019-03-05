d07ec00842e8782437c3ccf1ad4ca2a4c871df48
matao <muxter.matao@gmail.com>
2019-03-05 14:40:14 +0800

    test(Q02-CheckPermutation): add test cases for CheckPermutation

--- src/test/java/com/matao/chapter01/Q02_CheckPermutationTest.java
+++ src/test/java/com/matao/chapter01/Q02_CheckPermutationTest.java
@@ -1,4 +1,27 @@
 package com.matao.chapter01;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class Q02_CheckPermutationTest {
+
+    @Test
+    void testNull() {
+        Assertions.assertFalse(Q02_CheckPermutation.permutation(null, null));
+    }
+
+    @Test
+    void testEmptyString() {
+        Assertions.assertTrue(Q02_CheckPermutation.permutation("", ""));
+    }
+
+    @Test
+    void testDifferentLengthString() {
+        Assertions.assertFalse(Q02_CheckPermutation.permutation("abc", "abcc"));
+    }
+
+    @Test
+    void testLongCase() {
+        Assertions.assertTrue(Q02_CheckPermutation.permutation("Nice to meet you", "you ot meet ecNi"));
+    }
 }
